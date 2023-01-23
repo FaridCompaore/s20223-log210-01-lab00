562325dfe18db5d15c7bf300f970bd4ed0da7918
“FaridCompaore” <faridfreestyle11@outlook.com>
2023-01-22 19:22:26 -0500

    Up

--- test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/lab0/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -3,6 +3,20 @@ import { assert } from 'console';
 import 'jest-extended';
 
 describe('redemarrerJeu.test.ts', () => {
+
+  let j1,j2;
+
+  beforeAll(async () => {
+    j1 = {nom: "F"}
+    j2 = {nom: "A"}
+  });
+  describe('GET /api/v1/jeu/redemarrerJeu', () => {
+    test('returns status 200', async () => {
+      //const response = await axios.get('/api/v1/jeu/redemarrerJeu');
+      //expect(response.status).toBe(200);
+    });
+  });
+
   it("should implement test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
